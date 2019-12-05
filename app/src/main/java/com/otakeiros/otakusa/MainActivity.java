ea38cee8f339dfc612922540649a310fefe4d4fb
Thouma <jonasdiego16@gmail.com>
2019-12-05 18:18:49 -0200

    Criação das telas gerenciar fansub e cadastrar anime

--- app/src/main/java/com/otakeiros/otakusa/MainActivity.java
+++ app/src/main/java/com/otakeiros/otakusa/MainActivity.java
@@ -7,7 +7,9 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import com.otakeiros.otakusa.activitys.CadastrarAnimeActivity;
 import com.otakeiros.otakusa.activitys.CadastrarUsuarioActivity;
+import com.otakeiros.otakusa.activitys.GerenciarFansub;
 
 import java.util.ArrayList;
 
@@ -17,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(new Intent(getApplicationContext(), CadastrarUsuarioActivity.class));
+        startActivity(new Intent(getApplicationContext(), CadastrarAnimeActivity.class));
 
 
         Spinner spinnerLetra = findViewById(R.id.sp_alfabeto);
