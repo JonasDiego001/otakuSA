6b7d1f30f1950533b7469d830f1ab8448e5b3cd3
Thouma <jonasdiego16@gmail.com>
2019-12-06 11:12:58 -0200

    marge

---
+++ app/src/main/java/com/otakeiros/otakusa/activitys/Login.java
@@ -0,0 +1,41 @@
+package com.otakeiros.otakusa.activitys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.otakeiros.otakusa.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void confirmar_login(View view) {
+        String login;
+        String senha;
+        EditText et_login = findViewById(R.id.et_login_login);
+        EditText et_senha = findViewById(R.id.et_senha_login);
+
+        if (et_login.getText().toString().equals(""))
+            et_login.requestFocus();
+        else if (et_senha.getText().toString().equals(""))
+            et_senha.requestFocus();
+        else{
+            login = et_login.getText().toString();
+            senha = et_senha.getText().toString();
+        }
+    }
+
+    public void cadastrar(View view) {
+        
+    }
+}
---
+++ app/src/main/java/com/otakeiros/otakusa/entidades/Permisao.java
@@ -0,0 +1,6 @@
+package com.otakeiros.otakusa.entidades;
+
+public class Permisao {
+    private String nome;
+    private Integer AnoLancamento;
+}
