9f64666c118b2c5379b0146cdecd26554e2ee95d
Thouma <jonasdiego16@gmail.com>
2019-12-06 12:34:01 -0200

    marge

--- app/src/main/java/com/otakeiros/otakusa/activitys/GerenciarFansub.java
+++ app/src/main/java/com/otakeiros/otakusa/activitys/GerenciarFansub.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 
 import com.otakeiros.otakusa.R;
+
+import java.util.ArrayList;
 
 public class GerenciarFansub extends AppCompatActivity {
 
@@ -14,6 +18,20 @@ public class GerenciarFansub extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gerenciar_fansub);
+        ListView listaFansub = (ListView) findViewById(R.id.lista_fansub);
+        ArrayList<String> fansubs = preencherDados();
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, fansubs);
+        listaFansub.setAdapter(arrayAdapter);
+    }
+
+    private ArrayList<String> preencherDados(){
+        ArrayList<String> dados = new ArrayList<String>();
+        dados.add("Fansub1");
+        dados.add("Fansub2");
+        dados.add("Fansub3");
+        dados.add("Fansub2");
+        dados.add("Fansub3");
+        return  dados;
     }
 
     public void atualiza(View view) {
@@ -25,4 +43,6 @@ public class GerenciarFansub extends AppCompatActivity {
             nome = et_nome.getText().toString();
         }
     }
+
+
 }
